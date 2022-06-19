aef56ded343cc1b6e2b6f8c13e52be284c0ec42c
caiquebslima <caiquebslima@gmail.com>
2022-06-19 08:39:25 -0300

    adding proxy to api

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+    title = 'frontend'
+
+    constructor(private httpClient: HttpClient) {}
+
+    ngOnInit() {
+        this.httpClient
+            .post('/api/users/login', {
+                email: 'caique@gmail.com',
+                password: '1234abcd',
+            })
+            .subscribe(res => console.log(res))
+    }
 }
